bcd72653117bd6599f2c092c22153fe98dadd876
gogi2055 <gogiot2007@gmail.com>
2024-06-21 22:30:37 +0400

    first seventh

---
+++ day 6/classwork.py
@@ -0,0 +1,25 @@
+
+#task 1 
+
+name=input("Enter your name: ")
+print(f"your name is {name}")
+
+
+#task 2 
+
+num1 = int(input("Enter your first number: "))
+num2 = int(input("Enter your second number: "))
+
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(num1/num2)
+
+#task 3
+
+name = input("Enter your name: ")
+lastname = input("Enter your lastname: ")
+age = input("Enter your age: ")
+
+print("Your name is {}. Your lastname is {}. Your age is {}".format(name,lastname,age))
+print(f"Your name is {name}. Your lastname is {lastname}. Your age is {age}")
